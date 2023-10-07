82a53796fd5e9081677c4793eaa3d7fd20caf452
Vishal Singh <141947475+Vishal48-21@users.noreply.github.com>
2023-10-07 20:10:31 +0530

    Create build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py

---
+++ Dynammic-Programming/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numOfArrays(self, n: int, m: int, k: int) -> int:
+        visited = {}
+        MOD = 10**9 + 7
+        
+        def dp(i, biggest, cost):
+            if i == n:
+                return cost == k
+            
+            if cost > k or k - cost > m - biggest or k - cost > n - i:
+                return 0
+            
+            if (i, biggest, cost) in visited:
+                return visited[(i, biggest, cost)]
+            
+            res = 0
+            for val in range(1, m + 1):
+                res =  (res + dp(i + 1, max(biggest, val), cost if val <= biggest else cost + 1) ) % MOD
+            
+            visited[(i, biggest,cost )] = res
+            return res
+        
+        return dp(0,0,0)
